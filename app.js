85306b50ca393d0346ec1c8b2332e712f911b56c
Abhi <abhishek@xyz.com>
2017-08-10 23:07:32 +0530

    R

--- app.js
+++ app.js
@@ -6,11 +6,20 @@ angular.module('NarrowItDownApp', [ ])
        .service( 'MenuSearchService', MenuSearchService)
   //     .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
 
-NarrowItDownController.$inject=['MenuSearchService'];
+NarrowItDownController.$inject=['MenuSearchService','$scope'];
 
-       function NarrowItDownController(MenuSearchService){
+       function NarrowItDownController(MenuSearchService,$scope){
          var narrow = this;
+
 narrow.getMatchedMenuItems = function (searchTerm){
+  narrow.foundItems=[];
+    MenuSearchService.foundItems=[];
+
+  if(searchTerm==undefined)
+  {$scope.x=1;
+    return;}
+$scope.x=0;
+
   narrow.foundItems= MenuSearchService.getMatchedMenuItems(searchTerm);
 console.log(narrow.foundItems);
 };
@@ -28,21 +37,21 @@ templateUrl:'abc.html',
 scope:{
   foundItem: '<',
   onRemove:'&'
-}
-/*  controller:FoundItemsDirectiveController,
+},
+  controller:FoundItemsDirectiveController,
   controllerAs:'ctrl',
   bindToController:true,
-*/
+
          };
          return ddo;
        }
-  /*     function FoundItemsDirectiveController(){
+       function FoundItemsDirectiveController(){
          var ctrl=this;
-         ctrl.onRemove=function(index){
-           console.log("k");
-         };
+         //ctrl.onRemove=function(index){
+           //ctrl.foundItem.splice(index,1);
+         //};
        }
-*/
+
 MenuSearchService.$inject=['$http'];
        function MenuSearchService($http){
          var service=this;
@@ -61,7 +70,7 @@ service.remove=function(index){
 
         service.found=result.data;
         for(var i=0;i<service.found.menu_items.length;i++)
-        if(service.found.menu_items[i].description.indexOf(searchTerm)>=0 ||  service.found.menu_items[i].name.indexOf(searchTerm)>=0)
+        if(service.found.menu_items[i].description.toLowerCase().indexOf(searchTerm.toLowerCase())>=0 ||  service.found.menu_items[i].name.toLowerCase().indexOf(searchTerm.toLowerCase())>=0)
         { //console.log("hi");
           var q = {
             name:service.found.menu_items[i].name,
